124be4ec810aa65bca1cf56b83c8ff1c69cbab43
fourside <fourside@gmail.com>
2022-02-26 23:45:57 +0900

    modal storybook

---
+++ src/components/modal/modal.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Modal } from "./modal";
+
+const componentMeta: ComponentMeta<typeof Modal> = {
+  title: Modal.name,
+  component: Modal,
+  argTypes: {},
+};
+
+export default componentMeta;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+  children: (
+    <div
+      style={{
+        width: 300,
+        height: 300,
+        backgroundColor: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: "8px",
+      }}
+    >
+      hello
+    </div>
+  ),
+};
