1598a67af312ad651ff4dc9b1ffda981b6ba6e7b
fourside <fourside@gmail.com>
2022-02-26 13:52:20 +0900

    radix-ui accordion

---
+++ src/components/accordion/accordion.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Accordion } from "./accordion";
+
+const componentMeta: ComponentMeta<typeof Accordion> = {
+  title: Accordion.name,
+  component: Accordion,
+  argTypes: {},
+};
+
+export default componentMeta;
+
+const Template: ComponentStory<typeof Accordion> = (args) => <Accordion {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+  title: "Normal Accordion",
+  header: <div>Normal Accordion</div>,
+  children: <div>hello</div>,
+};
