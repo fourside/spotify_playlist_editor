c568398bc9ac98f02d4350602af5b0ca9f67ee73
fourside <fourside@gmail.com>
2022-02-27 00:03:23 +0900

    fix height

--- src/components/model/saved-tracks/saved-tracks.css.ts
+++ src/components/model/saved-tracks/saved-tracks.css.ts
@@ -1,7 +1,7 @@
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
-  height: "calc(100% - 142px)",
+  height: "calc(100vh - 122px)",
   padding: "16px",
   overflowY: "scroll",
 });
