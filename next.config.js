4a8268cf5f31bbe35819fb941ad562c0984a6729
fourside <fourside@gmail.com>
2022-03-02 22:14:50 +0900

    lint src

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    src: ["src"],
+  },
 };
 
 module.exports = withVanillaExtract(nextConfig);
